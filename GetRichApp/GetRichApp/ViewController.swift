ee6b513199d34ff09a22fbbf429b6b82bba22140
Mauricio Rivero <rivero.mauricio@gmail.com>
2022-12-18 07:06:30 -0500

    First commit with the initial files

---
+++ GetRichApp/GetRichApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetRichApp
+//
+//  Created by Mauricio Rivero on 18/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
